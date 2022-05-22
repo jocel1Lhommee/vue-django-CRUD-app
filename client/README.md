c027383ed9fcee64a44cd2fd0d2139bd9073bf5b
Jocel1 <jocel1.lhommee@gmail.com>
2022-05-22 13:25:13 +0200

    routeur front exemple

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# vue-starter-project
+# client
 
 ## Project setup
 ```
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,49 +1,122 @@
 <template>
-  <div class="HelloWorld">
-    <h1>
-      Nom des Employés
-    </h1>
-    <br/>
-    <h2>JSON des employés</h2>
-    <p>{{employes}}</p>
-    <div v-for="(employe) in employes" :key="employe.id">
-      <h2>JSON de l'employé {{employe.id}}</h2>
-      <p>{{employe}}</p>
-    </div>
-    <h2>donée des employes</h2>
-    <p>{{employes}}</p>
-    <div v-for="(employe) in employes" :key="employe.id">
-      <h2>JSON de l'employé {{employe.id}}</h2>
-      <p>Nom: {{employe.name}}</p>
-      <p>Description: {{employe.description}}</p>
-    </div>
-    
-    
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+          target="_blank"
+          rel="noopener"
+          >router</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  name: 'HelloWorld',
-  data(){
-    return{
-      employes:null,
-    }
+  name: "HelloWorld",
+  props: {
+    msg: String,
   },
-  mounted(){
-    axios
-    .get('http://127.0.0.1:8000/employee/')
-    .then((reponse) => {
-      this.employes=reponse.data;
-      console.log(this.employes);
-      })
-  }
-
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
